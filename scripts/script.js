5b1ec332f9719e56bd190850e790fb2d47990b1c
YosuaniBG <yborgesg@gmail.com>
2022-12-21 13:07:48 -0500

    Actualizacion general

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     document.querySelector('.cart-img').addEventListener('click',function(){
         document.querySelector('.cart-container').classList.toggle('hidden-cart-container');
+        document.querySelector('body').classList.toggle('hidden-scroll');
     });
 }
