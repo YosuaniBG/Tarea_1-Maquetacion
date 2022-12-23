b03f2710acb83a563bc82561f3a59921224e5d24
YosuaniBG <yborgesg@gmail.com>
2022-12-22 22:37:12 -0500

    Offcanvas con DIV

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 window.onload = function(){
     document.querySelector('.cart-img').addEventListener('click',function(){
-        document.querySelector('.cart-container').classList.toggle('hidden-cart-container');
+        document.querySelector('.cart-container').classList.toggle('show-cart-container');
         document.querySelector('body').classList.toggle('hidden-scroll');
+        document.querySelector('.offcanvas-backdrop').classList.toggle('show-offcanvas-backdrop');
     });
+
 }
