c34da15d19a4ed47e4ceab18f056515feae4545e
YosuaniBG <yborgesg@gmail.com>
2023-01-05 22:41:01 -0500

    Menu lateral sin scroll horizontal

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 window.onload = function(){
     document.querySelector('.cart-img').addEventListener('click', () => {
         document.querySelector('.cart-container').classList.toggle('show-cart-container');
-        document.querySelector('body').classList.toggle('hidden-scroll');
+        document.querySelector('body').classList.toggle('hidden-scroll-y');
         document.querySelector('.offcanvas-backdrop').classList.toggle('show-offcanvas-backdrop');
     });
 
     document.querySelector('.fa-bars').addEventListener('click', () => {
         document.querySelector('.navbar').classList.toggle('navbar-show');
         document.querySelector('main').classList.toggle('main-translate');
+        document.querySelector('body').classList.toggle('hidden-scroll-x');
     });
 
 }
