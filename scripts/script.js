53fd9c9567bed052d508924dcdfbcc51e17a35b4
YosuaniBG <yborgesg@gmail.com>
2023-01-05 22:10:42 -0500

    Menu lateral actualizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    document.querySelector('.cart-img').addEventListener('click',function(){
+    document.querySelector('.cart-img').addEventListener('click', () => {
         document.querySelector('.cart-container').classList.toggle('show-cart-container');
         document.querySelector('body').classList.toggle('hidden-scroll');
         document.querySelector('.offcanvas-backdrop').classList.toggle('show-offcanvas-backdrop');
@@ -7,6 +7,7 @@ window.onload = function(){
 
     document.querySelector('.fa-bars').addEventListener('click', () => {
         document.querySelector('.navbar').classList.toggle('navbar-show');
+        document.querySelector('main').classList.toggle('main-translate');
     });
 
 }
